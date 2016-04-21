2cfb10f6c9d1fd412a1b5d9f8ea99b50754336d4
rodrigoadfaria <rodrigoadfaria@gmail.com>
2016-04-21 16:30:31 +0000

    Changing the default icons of the presentation

--- slide_config.js
+++ slide_config.js
@@ -1,18 +1,18 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'Title Goes Here<br>Up To Two Lines',
-    subtitle: 'Subtitle Goes Here',
-    //eventInfo: {
-    //  title: 'Google I/O',
-    //  date: '6/x/2013'
-    //},
+    title: 'Introdução ao Javascript',
+    subtitle: 'Seminário de Programação Orientada a Objetos',
+    eventInfo: {
+      title: '<br/>Introdução ao Javascript',
+      date: '28/04/2016'
+    },
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
     //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
-    favIcon: 'images/google_developers_logo_tiny.png',
+    favIcon: 'images/code_programming_js_128.png',
     fonts: [
       'Open Sans:regular,semibold,italic,italicsemibold',
       'Source Code Pro'
@@ -22,19 +22,19 @@ var SLIDE_CONFIG = {
 
   // Author information
   presenters: [{
-    name: 'Firstname Lastname',
-    company: 'Job Title<br>Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
-  }/*, {
-    name: 'Second Name',
+    name: 'Rodrigo Faria',
+    company: 'Especialista em Sistemas, Inatel',
+    twitter: '@rodrigoadfaria',
+    www: 'http://rodrigoadfaria.blogspot.com.br/',
+    github: 'http://github.com/rodrigoadfaria'
+  },
+  {
+    name: 'Rodolfo Scotolo',
     company: 'Job Title, Google',
     gplus: 'http://plus.google.com/1234567890',
     twitter: '@yourhandle',
     www: 'http://www.you.com',
     github: 'http://github.com/you'
-  }*/]
+  }]
 };
 
