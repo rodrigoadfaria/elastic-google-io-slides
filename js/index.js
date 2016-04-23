a2bba12ff1c0206f83a6021993423fb0ea5e11ac
rodrigoadfaria <rodrigoadfaria@gmail.com>
2016-04-23 14:14:02 +0000

    Adding content for the DOM model

--- js/index.js
+++ js/index.js
@@ -8,8 +8,24 @@ function init() {
     if (label) {
         label.addEventListener('click', helloWorld);
     }
+    
+    var btnChangeUl = document.getElementById('change-ul');
+    if (btnChangeUl) {
+        btnChangeUl.addEventListener('click', changeUlAppearence);
+    }    
 };
 
 function helloWorld() {
     alert('Mantenha o JavaScript discreto :)');
+};
+
+function changeUlAppearence() {
+    var mutableUl = document.getElementById('ul-mutable');
+    if (mutableUl) {
+        if (mutableUl.className.match(/english/)) {
+            mutableUl.className = mutableUl.className.replace('english', '');
+        } else {
+            mutableUl.className = mutableUl.className.trim().concat(' english');
+        }
+    }
 };
